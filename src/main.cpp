d7df413ce11d412b20fe90d4b6e25368c475be1d
Shilyaev Egor <egor.shilyaev@gmail.com>
2017-06-28 20:10:39 +0500

    refact: Readme

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@ int main(int argc, char *argv[])
   QQmlApplicationEngine engine;
   engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
   if (engine.rootObjects().isEmpty())
+  {
     return -1;
+  }
 
   return app.exec();
 }
