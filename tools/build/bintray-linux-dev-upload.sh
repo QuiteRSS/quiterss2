0e99180119c9b89a1dfbcab97da1882c0f744e23
Shilyaev Egor <egor.shilyaev@gmail.com>
2017-07-02 13:41:35 +0500

    change: Конфиг для сборки в travis-ci (test20)

--- tools/build/bintray-linux-dev-upload.sh
+++ tools/build/bintray-linux-dev-upload.sh
@@ -5,11 +5,11 @@ set -x
 
 user_info="$BINTRAY_USER:$BINTRAY_API_KEY"
 repo="quiterss2-development"
-package="MacOSX-x64-dev"
+package="Linux-x64-dev"
 build_dir="."
 build_ver="build${TRAVIS_BUILD_NUMBER}-${TRAVIS_BRANCH}"
-file_path="QuiteRSS-$build_ver.dmg"
-build_file="$(ls ${build_dir}/*.dmg | head -n 1)"
+file_path="QuiteRSS-$build_ver.AppImage"
+build_file="$(ls ${build_dir}/*.AppImage | head -n 1)"
 upload_url="https://api.bintray.com/content/quiterss/$repo/$package/$build_ver/$file_path?override=1&publish=1"
 
 if [ -z "$build_file" ]; then
--- tools/build/bintray-macosx-upload.sh
+++ tools/build/bintray-macosx-upload.sh
@@ -4,10 +4,10 @@ set -ev
 set -x
 
 user_info="$BINTRAY_USER:$BINTRAY_API_KEY"
-repo="quiterss2-development"
-package="MacOSX-x64-dev"
+repo="quiterss2"
+package="MacOSX-x64"
 build_dir="."
-build_ver="build${TRAVIS_BUILD_NUMBER}-${TRAVIS_BRANCH}"
+build_ver="${TRAVIS_TAG}"
 file_path="QuiteRSS-$build_ver.dmg"
 build_file="$(ls ${build_dir}/*.dmg | head -n 1)"
 upload_url="https://api.bintray.com/content/quiterss/$repo/$package/$build_ver/$file_path?override=1&publish=1"
