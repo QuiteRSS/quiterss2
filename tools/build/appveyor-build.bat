f0ad3558b644cd8f025da548bac1ea6de18d0130
Shilyaev Egor <egor.shilyaev@gmail.com>
2017-07-08 17:24:19 +0500

    change: Конфиг для сборки в appveyor (тест44)

--- tools/build/appveyor-build.bat
+++ tools/build/appveyor-build.bat
@@ -6,19 +6,19 @@ mkdir build && cd build
 qmake CONFIG+=%CONFIGURATION% INCLUDEPATH+="%OPENSSL_DIR%\include" LIBS+=-L%OPENSSL_DIR%\lib ..\quiterss2.pro
 call jom
 windeployqt bin\quiterss.exe --qmldir=..\resources\qml
-cp c:\Windows\SysWOW64\msvcp140.dll .\bin
-cp c:\Windows\SysWOW64\vccorlib140.dll .\bin
-cp c:\Windows\SysWOW64\vcruntime140.dll .\bin
-cp %OPENSSL_DIR%\libeay32.dll .\bin
-cp %OPENSSL_DIR%\ssleay32.dll .\bin
-cp ..\AUTHORS .\bin
-cp ..\CHANGELOG .\bin
-cp ..\LICENSE .\bin
-cp ..\README.md .\bin
+cp c:\Windows\SysWOW64\msvcp140.dll bin
+cp c:\Windows\SysWOW64\vccorlib140.dll bin
+cp c:\Windows\SysWOW64\vcruntime140.dll bin
+cp %OPENSSL_DIR%\libeay32.dll bin
+cp %OPENSSL_DIR%\ssleay32.dll bin
+cp ../AUTHORS bin
+cp ../CHANGELOG bin
+cp ../LICENSE bin
+cp ../README.md bin
 if "%PORTABLE%" == "true" (
     mkdir QuiteRSS-%APPVEYOR_BUILD_VERSION%
-    cp -r bin\* QuiteRSS-%APPVEYOR_BUILD_VERSION%
-    echo > QuiteRSS-%APPVEYOR_BUILD_VERSION%\portable.dat
+    cp -r bin/* QuiteRSS-%APPVEYOR_BUILD_VERSION%/
+    type nul > QuiteRSS-%APPVEYOR_BUILD_VERSION%\portable.dat
     7z a "QuiteRSS-%APPVEYOR_BUILD_VERSION%.zip" "QuiteRSS-%APPVEYOR_BUILD_VERSION%"
 ) else (
     if "%APPVEYOR_REPO_TAG%" == "true" (
