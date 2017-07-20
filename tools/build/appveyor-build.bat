436988377222c2497843931d08048162ae9d1be1
Shilyaev Egor <egor.shilyaev@gmail.com>
2017-07-21 00:08:14 +0500

    change: Каталог Qml для развертывания в Windows и Mac OS X

--- tools/build/appveyor-build.bat
+++ tools/build/appveyor-build.bat
@@ -5,7 +5,8 @@ echo "BUILD %APPVEYOR_BUILD_VERSION%_%CONFIGURATION%_%ARCH%_%APPVEYOR_REPO_TAG_N
 mkdir build && cd build
 qmake CONFIG+=%CONFIGURATION% INCLUDEPATH+="%OPENSSL_DIR%\include" LIBS+=-L%OPENSSL_DIR%\lib ..\quiterss2.pro
 call jom
-windeployqt bin\quiterss.exe --qmldir=..\resources\qml\desktop --no-translations --no-compiler-runtime
+rmdir /s /q  ..\..\resources\qml\+mobile;
+windeployqt bin\quiterss.exe --qmldir=..\resources\qml --no-translations --no-compiler-runtime
 cp c:\Windows\SysWOW64\msvcp140.dll bin
 cp c:\Windows\SysWOW64\vccorlib140.dll bin
 cp c:\Windows\SysWOW64\vcruntime140.dll bin
